d77890b174574807bcd58754c927f7afa68380ec
KEVINC2017 <kev201725@gmail.com>
2023-10-29 18:17:15 -0500

    modified:   api_functions.py

--- api_functions.py
+++ api_functions.py
@@ -68,7 +68,7 @@ def userdata(user_id):
     porcentaje_recomendacion = user_data['percentage_recommend'].values[0]
     cantidad_items = user_data['count_items'].values[0]
 
-    return {"Usuario": user_id, "Dinero gastado": str(dinero_gastado)+" USD"}
+    return {"Usuario": user_id, "Dinero gastado": str(dinero_gastado)+" USD", "% de recomendación": str(porcentaje_recomendacion) + " %"}
     #return {"Usuario": user_id, "Dinero gastado": str(dinero_gastado)+" USD", "% de recomendación": str(porcentaje_recomendacion) + " %","cantidad de items": cantidad_items}
 
 def UserForGenre(genre):
