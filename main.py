ea85969bc63ef1215b681171996cb8935de001a2
KEVINC2017 <kev201725@gmail.com>
2023-10-30 19:18:16 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -73,12 +73,13 @@ async def recomendacion_juego(item_id: str = Query (...,
     '''Muestra una lista con 5 juegos recomendados similares al ingresado
     '''
     return fa.recomendacion_juego(item_id)
-
+'''
 #7 recomendacion_usuario
 @app.get(path= "/recomendacion_usuario")
 async def recomendacion_usuario(user_id: str = Query (...,
                                             description='Identificador único del usuario',
                                             example='-2SV-vuLB-Kg')):
-    '''Muestra una lista con 5 juegos recomendados para el usuario ingresado
-    '''
+        #Muestra una lista con 5 juegos recomendados para el usuario ingresado
+
     return fa.recomendacion_usuario(user_id)
+'''
